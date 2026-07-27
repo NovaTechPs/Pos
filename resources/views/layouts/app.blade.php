19ec057d1a1ed2237086b84dd93c9bb4adb71ea0
ZeyadHawash9 <148364616+ZeyadHawash9@users.noreply.github.com>
2026-07-27 22:09:37 +0300

    edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,13 @@
-<x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts::app.sidebar>
+@if (request()->is('admin*'))
+    <x-layouts::app.saas-sidebar :title="$title ?? null">
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+    </x-layouts::app.saas-sidebar>
+@else
+    <x-layouts::app.sidebar :title="$title ?? null">
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+    </x-layouts::app.sidebar>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,9 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
-    Route::livewire('/post/create', 'pages::post.create');
-    Route::livewire('/role/create', 'pages::roles.create-role');
-        Route::livewire('/role', 'pages::roles.role');
-
 
 });
 
-Route::get('/pos', function () {
-    return view('pos');
-});
 
 
 require __DIR__.'/settings.php';
