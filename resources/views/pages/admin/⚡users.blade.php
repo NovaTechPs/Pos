0e11b97d4817ef711e94948bb9a1cc6134e1557a
ZeyadHawash9 <148364616+ZeyadHawash9@users.noreply.github.com>
2026-09-11 20:17:14 +0300

    test

--- resources/views/pages/admin/⚡users.blade.php
+++ resources/views/pages/admin/⚡users.blade.php
@@ -147,7 +147,7 @@ new class extends Component {
                     'name'      => $this->tenant_name,
                     'domain'    => $this->domain,
                     'plan_id'   => $this->plan_id, // ربط المتجر بالباقة المختارة
-                    'is_active' => $this->is_active,
+                    'is_active' => true,
                     'owner_id'  => $ownerId,
                 ]);
             }
