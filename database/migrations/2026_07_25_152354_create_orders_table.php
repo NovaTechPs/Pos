bf8391ff6733c249bed0bd71c5d59c81dd9175ac
ZeyadHawash9 <148364616+ZeyadHawash9@users.noreply.github.com>
2026-09-13 23:02:43 +0300

    edit

--- database/migrations/2026_07_25_152354_create_orders_table.php
+++ database/migrations/2026_07_25_152354_create_orders_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+                        Schema::disableForeignKeyConstraints();
+
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('tenant_id')->constrained()->cascadeOnDelete();
